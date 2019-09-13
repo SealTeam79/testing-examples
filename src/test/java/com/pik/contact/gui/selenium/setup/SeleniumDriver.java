0c3979a0ec4f0f84b61a2766dec495995b39a1ce
nitin.parashar <nitin.parashar@servicenow.com>
2019-09-13 16:37:16 -0700

    switch to chrome driver

--- src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
+++ src/test/java/com/pik/contact/gui/selenium/setup/SeleniumDriver.java
@@ -1,8 +1,11 @@
 package com.pik.contact.gui.selenium.setup;
 
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumDriver {
@@ -11,7 +14,15 @@ public class SeleniumDriver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new FirefoxDriver();    //can be replaced with HtmlUnitDriver for better performance
+            //File file = new File("lib" + File.separator + "chromedriver.exe");
+            //System.setProperty("webdriver.chrome.driver", "/Users/nitin" +
+            //                                              ".parashar/Downloads" +
+            //                                              "/selenium_test_drivers/chromedriver");
+
+            System.setProperty("webdriver.chrome.driver","chromedriver");
+
+            driver = new ChromeDriver();    //can be replaced with HtmlUnitDriver for better performance
+
             driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         }
         return driver;
